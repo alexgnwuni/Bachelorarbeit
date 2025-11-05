688d3048c910bf2098ff3c141991abed7ac4b88a
Alexander Günnewig <alexander.guennewig@outlook.de>
2025-11-05 11:25:19 +0100

    included a new page for the study

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Introduction from "./pages/Introduction";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import Study from "./pages/Study";
 
 const queryClient = new QueryClient();
 
@@ -13,6 +14,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Introduction />} />
+          <Route path="/study" element={<Study />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
